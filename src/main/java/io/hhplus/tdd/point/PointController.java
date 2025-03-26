c6b64151eb1b7bb16a8b5a09c62340726cabe6c1
Jeongyeon Kim <wjddus9132@gmail.com>
2025-03-27 00:10:18 +0900

    feat: 각 Controller 메서드의 사용자 아이디와 충전/사용 포인트 validaiton 추가

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -1,12 +1,15 @@
 package io.hhplus.tdd.point;
 
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/point")
@@ -17,30 +20,30 @@ public class PointController {
 
     @GetMapping("{id}")
     public UserPoint point(
-            @PathVariable long id
+            @PathVariable @Positive(message = "사용자 아이디는 양수입니다.") long id
     ) {
         return pointService.getUserPoint(id);
     }
 
     @GetMapping("{id}/histories")
     public List<PointHistory> history(
-            @PathVariable long id
+            @PathVariable @Positive(message = "사용자 아이디는 양수입니다.") long id
     ) {
         return pointService.getUserPointHistory(id);
     }
 
     @PatchMapping("{id}/charge")
     public UserPoint charge(
-            @PathVariable long id,
-            @RequestBody long amount
+            @PathVariable @Positive(message = "사용자 아이디는 양수입니다.") long id,
+            @RequestBody @Positive(message = "충전할 포인트는 양수입니다.") long amount
     ) {
         return pointService.chargePoint(id, amount);
     }
 
     @PatchMapping("{id}/use")
     public UserPoint use(
-            @PathVariable long id,
-            @RequestBody long amount
+            @PathVariable @Positive(message = "사용자 아이디는 양수입니다.") long id,
+            @RequestBody @Positive(message = "사용할 포인트는 양수입니다.") long amount
     ) {
         return pointService.usePoint(id, amount);
     }
