1565712433e9521454e4ddfd3634d06cfe44882c
Jeongyeon Kim <wjddus9132@gmail.com>
2025-03-26 23:17:51 +0900

    feat: 포인트 관련 예외 처리 핸들러 추가

--- src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -1,5 +1,9 @@
 package io.hhplus.tdd;
 
+import io.hhplus.tdd.exception.InsufficientPointException;
+import io.hhplus.tdd.exception.NonPositiveChargeAmountException;
+import io.hhplus.tdd.exception.NonPositiveUseAmountException;
+import io.hhplus.tdd.exception.PointLimitExceededException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -7,6 +11,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 class ApiControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {
+            InsufficientPointException.class,
+            NegativeArraySizeException.class,
+            NonPositiveChargeAmountException.class,
+            NonPositiveUseAmountException.class,
+            PointLimitExceededException.class}
+    )
+    public ResponseEntity<ErrorResponse> handleCustomException(Exception e) {
+        return ResponseEntity.status(400)
+                .body(new ErrorResponse("400", e.getMessage()));
+    }
+
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
