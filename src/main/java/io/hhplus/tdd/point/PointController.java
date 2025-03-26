f0bd21920b3cae0e7525840269951c10839b9ca2
Jeongyeon Kim <wjddus9132@gmail.com>
2025-03-26 22:24:15 +0900

    feat: 유저 포인트 사용 Controller 구현

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -37,14 +37,11 @@ public class PointController {
         return pointService.chargePoint(id, amount);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-     */
     @PatchMapping("{id}/use")
     public UserPoint use(
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointService.usePoint(id, amount);
     }
 }
