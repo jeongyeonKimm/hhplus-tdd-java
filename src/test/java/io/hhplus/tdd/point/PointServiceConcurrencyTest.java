349201bc37e1ba5cca9195c1580ddcde7cb241bf
Jeongyeon Kim <wjddus9132@gmail.com>
2025-03-28 00:58:11 +0900

    refactor: 포인트 충전/사용 이력 확인 로직 추가

--- src/test/java/io/hhplus/tdd/point/PointServiceConcurrencyTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceConcurrencyTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -25,13 +26,14 @@ class PointServiceConcurrencyTest {
     @Autowired
     private PointHistoryRepository pointHistoryRepository;
 
-    @DisplayName("한 명의 유저가 100번 충전 요청을 하면 충전 금액 * 100 만큼의 포인트가 충전된다.")
+    @DisplayName("충전/사용 이력이 없는 한 명의 유저가 100번 충전 요청을 하면 충전 금액 * 100 만큼의 포인트가 충전된다.")
     @Test
     void chargePoint_concurrently() throws InterruptedException {
         int threadCount = 100;
         long userId = 1L;
+        long initialAmount = 0L;
         long chargeAmount = 1000L;
-        userPointRepository.saveUserPoint(userId, 0L);
+        userPointRepository.saveUserPoint(userId, initialAmount);
 
         ExecutorService executor = Executors.newFixedThreadPool(10);
         CountDownLatch latch = new CountDownLatch(threadCount);
@@ -49,10 +51,13 @@ class PointServiceConcurrencyTest {
         latch.await();
 
         UserPoint userPoint = userPointRepository.findUserPointById(userId);
-        assertThat(userPoint.point()).isEqualTo(chargeAmount * threadCount);
+        assertThat(userPoint.point()).isEqualTo(initialAmount + (chargeAmount * threadCount));
+
+        List<PointHistory> histories = pointHistoryRepository.findPointHistoriesByUserId(userId);
+        assertThat(histories).hasSize(threadCount);
     }
 
-    @DisplayName("한 명의 유저가 100번 사용 요청을 하면 사용 금액 * 100 만큼의 포인트가 차감된다.")
+    @DisplayName("충전/사용 이력이 없는 한 명의 유저가 100번 사용 요청을 하면 사용 금액 * 100 만큼의 포인트가 차감된다.")
     @Test
     void usePoint_concurrently() throws InterruptedException {
         int threadCount = 100;
@@ -78,5 +83,8 @@ class PointServiceConcurrencyTest {
 
         UserPoint userPoint = userPointRepository.findUserPointById(userId);
         assertThat(userPoint.point()).isEqualTo(initialAmount - (useAmount * threadCount));
+
+        List<PointHistory> histories = pointHistoryRepository.findPointHistoriesByUserId(userId);
+        assertThat(histories).hasSize(threadCount);
     }
 }
