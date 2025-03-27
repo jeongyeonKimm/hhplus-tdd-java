2052371d50bea052f1ec245af0223588d0c3753e
Jeongyeon Kim <wjddus9132@gmail.com>
2025-03-28 00:49:10 +0900

    test: 포인트 충전/사용 동시성 테스트

---
+++ src/test/java/io/hhplus/tdd/point/PointServiceConcurrencyTest.java
@@ -0,0 +1,82 @@
+package io.hhplus.tdd.point;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@SpringBootTest
+class PointServiceConcurrencyTest {
+
+    @Autowired
+    private PointService pointService;
+
+    @Autowired
+    private UserPointRepository userPointRepository;
+
+    @Autowired
+    private PointHistoryRepository pointHistoryRepository;
+
+    @DisplayName("한 명의 유저가 100번 충전 요청을 하면 충전 금액 * 100 만큼의 포인트가 충전된다.")
+    @Test
+    void chargePoint_concurrently() throws InterruptedException {
+        int threadCount = 100;
+        long userId = 1L;
+        long chargeAmount = 1000L;
+        userPointRepository.saveUserPoint(userId, 0L);
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executor.submit(() -> {
+                try {
+                    pointService.chargePoint(userId, chargeAmount);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        UserPoint userPoint = userPointRepository.findUserPointById(userId);
+        assertThat(userPoint.point()).isEqualTo(chargeAmount * threadCount);
+    }
+
+    @DisplayName("한 명의 유저가 100번 사용 요청을 하면 충전 금액 * 100 만큼의 포인트가 차감된다.")
+    @Test
+    void usePoint_concurrently() throws InterruptedException {
+        int threadCount = 100;
+        long userId = 1L;
+        long initialAmount = 300_000L;
+        long useAmount = 1000L;
+        userPointRepository.saveUserPoint(userId, initialAmount);
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executor.submit(() -> {
+                try {
+                    pointService.usePoint(userId, useAmount);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        UserPoint userPoint = userPointRepository.findUserPointById(userId);
+        assertThat(userPoint.point()).isEqualTo(initialAmount - (useAmount * threadCount));
+    }
+}
